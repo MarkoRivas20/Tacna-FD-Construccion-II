697462a3efe277c718a92b568cfbd88d31a6e112
Ronald Ordoñez <37679541+ronordonezq@users.noreply.github.com>
2020-11-04 16:52:06 -0500

    Corrección de Estándar de Programación
    revisión y corrección de Estándar de Programación, y agregación de Actividad y Fragment de "ListarPedido"

---
+++ Proyecto_ConstruccionII/TacnaFDBusiness/app/src/main/java/com/example/tacnafdbusiness/vista/ListarPedido_Vista.java
@@ -0,0 +1,23 @@
+package com.example.tacnafdbusiness.vista;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tacnafdbusiness.R;
+
+
+public class ListarPedido_Vista extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_listar_pedido__vista, container, false);
+    }
+}
--- Proyecto_ConstruccionII/TacnaFDBusiness/app/src/main/java/com/example/tacnafdbusiness/vista/OpcionesEstablecimiento_Vista.java
+++ Proyecto_ConstruccionII/TacnaFDBusiness/app/src/main/java/com/example/tacnafdbusiness/vista/OpcionesEstablecimiento_Vista.java
@@ -34,6 +34,7 @@ public class OpcionesEstablecimiento_Vista extends Fragment implements OpcionesE
     ImageView ImgResenas;
     ImageView ImgMenu;
     ImageView ImgCupon;
+    ImageView ImgPedidos;
     ImageView ImgMetodosPago;
 
     ModificarEstablecimiento_Vista modificarEstablecimiento_vista;
@@ -42,6 +43,7 @@ public class OpcionesEstablecimiento_Vista extends Fragment implements OpcionesE
     ListarResena_Vista listarResena_vista;
     ListarItemMenu_Vista listarItemMenu_vista;
     ListarCupon_Vista listarCupon_vista;
+    ListarPedido_Vista listarPedido_vista;
     ConfigurarMetodosPago_Vista configurarMetodosPago_vista;
 
     @Override
@@ -58,6 +60,7 @@ public class OpcionesEstablecimiento_Vista extends Fragment implements OpcionesE
         ImgResenas = (ImageView) view.findViewById(R.id.ImgResenas);
         ImgMenu = (ImageView) view.findViewById(R.id.ImgMenu);
         ImgCupon = (ImageView) view.findViewById(R.id.ImgCupon);
+        ImgPedidos = (ImageView) view.findViewById(R.id.ImgPedidos);
         ImgMetodosPago = (ImageView) view.findViewById(R.id.ImgMetodosPago);
 
         modificarEstablecimiento_vista = new ModificarEstablecimiento_Vista();
@@ -66,6 +69,8 @@ public class OpcionesEstablecimiento_Vista extends Fragment implements OpcionesE
         listarResena_vista = new ListarResena_Vista();
         listarItemMenu_vista = new ListarItemMenu_Vista();
         listarCupon_vista = new ListarCupon_Vista();
+        listarPedido_vista = new ListarPedido_Vista();
+
         configurarMetodosPago_vista = new ConfigurarMetodosPago_Vista();
 
         mPresenter=new OpcionesEstablecimiento_Presentador(this);
@@ -111,6 +116,13 @@ public class OpcionesEstablecimiento_Vista extends Fragment implements OpcionesE
             @Override
             public void onClick(View v) {
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmento, listarCupon_vista).addToBackStack(null).commit();
+            }
+        });
+
+        ImgPedidos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmento, listarPedido_vista).addToBackStack(null).commit();
             }
         });
 
